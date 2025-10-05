0dc82e579f2835be799f14ced202e5108f53f49f
Malik Kernel <dm372842@gmail.com>
2025-10-05 16:05:47 +0300

    ...

--- users_manager/settings.py
+++ users_manager/settings.py
@@ -128,8 +128,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='orsolisback@gmail.com'
-EMAIL_HOST_PASSWORD="hafwlqylvublvcnt"
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=""
 DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
 
 
